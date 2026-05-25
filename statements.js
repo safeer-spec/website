0309fb65f1c2bd82835db5dd7b4738a2244f93ef
safeer-spec <piratesstrawhat282@gmail.com>
2026-05-25 21:36:17 +0530

    Add files via upload

---
+++ statements.js
@@ -0,0 +1,90 @@
+// let num1 = 1
+// let num2 = 30
+
+// if(num1 < num2){
+// console.log("num1 is big")
+// }
+// else{
+// console.log("num2 is big")
+// }
+
+
+// age = 18
+// if (age<20){
+// console.log("eligable")
+// } 
+// else{
+//     console.log("not eligable")
+// }
+
+// let age = 22
+// let hasID = true
+// if (age>18){
+//     if(hasID){
+//         console.log("entry allowed")
+//     }
+//     else{
+//         console.log("not allowed")
+//     }
+// }
+// else{
+//     console.log("under age, not allowed")
+// }
+
+
+// let grade = "d"
+// switch (grade){
+//     case "a" :
+//     console.log("achieved 90 per above")
+//     break;
+
+//     case "b":
+//         console.log("achieved 75 per above")
+// break;
+
+
+// case "c":
+// console.log("just passed - need improvement")
+// break;
+
+// case "d":
+//     console.log("failed")
+
+// }
+
+let num = (1)
+if (num>0){
+    console.log("number is positive")
+}
+
+else if 
+(num<0){
+console.log("number is negative")
+}
+
+
+let number = 30
+if (num>30){
+    console.log("number is even")
+}
+
+else if 
+(num<30){
+console.log("number is odd")
+}
+
+
+
+let mark = 90
+if (mark>80)
+    console.log("passed")
+else if (mark<50){
+    console.log("just passed")
+}
+else{
+ console.log("failed")
+}
+
+    
+
+
